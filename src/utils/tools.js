98913685d537ff34d9f2474de341b31d56bed4a7
yulin <2247586191@qq.com>
2023-07-26 23:16:19 +0800

    :recycle: refactor(custom): 修改工具函数的名字

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -3,7 +3,7 @@
  * @param {Number} num 
  * @returns 
  */
-function U_checkEvenOdd(num) {
+function tools_checkEvenOdd(num) {
   if (typeof num !== 'number') {
     throw new Error('num must be Number --- checkEvenOdd');
   }
@@ -13,5 +13,5 @@ function U_checkEvenOdd(num) {
   return num % 2 === 0 ? 'even' : 'odd';
 }
 
-export { U_checkEvenOdd };
+export { tools_checkEvenOdd };
 
