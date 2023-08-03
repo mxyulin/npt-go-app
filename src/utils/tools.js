66f182b8e48d557c693eea43410ac20dad46989e
yulin <2247586191@qq.com>
2023-08-03 14:51:50 +0800

    :sparkles: feat(custom): 添加两个经典的工具函数：防抖和节流

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -13,5 +13,55 @@ function tools_checkEvenOdd(num) {
   return num % 2 === 0 ? 'even' : 'odd';
 }
 
-export { tools_checkEvenOdd };
+/**
+ * 防抖
+ * @param {Function} func 
+ * @param {Number} wait 
+ * @param {Boolean} immediate 立刻执行
+ * @returns 
+ */
+function tools_debounce(func, wait, immediate) {
+  let timeout; // 定义一个变量来存储定时器的 ID
+
+  return function () {
+    const context = this, // 保存 this 的值
+       args = arguments; // 保存函数的参数
+
+    const later = function () { // 定义一个函数，用于在 wait 时间后执行 func 函数
+      timeout = null; // 清空定时器的 ID
+      if (!immediate) func.apply(context, args); // 如果 immediate 为 false，则执行 func 函数
+    };
+
+    clearTimeout(timeout); // 清空定时器
+    timeout = setTimeout(later, wait); // 设置定时器，wait 时间后执行 later 函数
+
+    const callNow = immediate && !timeout; // 如果 immediate 为 true 并且 timeout 为 null，则立即执行 func 函数
+    if (callNow) func.apply(context, args); // 如果 callNow 为 true，则立即执行 func 函数
+  };
+}
+
+/**
+ * 节流
+ * @param {Function} func 
+ * @param {Number} wait 
+ * @returns 
+ */
+function tools_throttle(func, wait) {
+  let timeout; // 定义一个变量来存储定时器的 ID
+
+  return function () {
+    const context = this, // 保存 this 的值
+      args = arguments; // 保存函数的参数
+
+    if (!timeout) { // 如果不处于节流状态，则执行 func 函数，wait 时间后将 timeout 设为 null
+      timeout = setTimeout(() => {
+        clearTimeout(timeout);
+        timeout = null;
+        func.apply(context, args);
+      }, wait);
+    }
+  };
+}
+
+export { tools_checkEvenOdd, tools_debounce, tools_throttle };
 
