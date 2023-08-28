293763189ae56437f7e5e45d7f15b70c0d912568
yulin <2247586191@qq.com>
2023-08-28 18:23:11 +0800

    :sparkles: feat(custom): 添加了开发环境用的代理服务器配置

--- vite.config.js
+++ vite.config.js
@@ -64,13 +64,15 @@ export default defineConfig({
   server: {
     host: 'localhost',
     port: 8000,
-    strictPort: true, // 严格端口
+    strictPort: true, // 若端口已被占用则会直接退出
+    https: false,// 不启用 TLS + HTTP/2
+    open: false,// 不自动打开到浏览器（浏览器可通过设置环境变量 process.env.BROWSER配置）
     proxy: {
-      '/socket': {
-        target: 'ws://192.168.2.64:8090',
-        ws: true,
-        rewrite: (path) => path.replace(/^\/socket/, '')
+      '/api': {
+        target: 'https://randomuser.me/',
+        changeOrigin: true,// 跨源
       }
-    }
+    },
+    cors: true,// 启用配置开发服务器 CORS 策略
   }
 })
