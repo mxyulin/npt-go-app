ed1d992a24965d092443fd8ac98429c372a01916
yulin <2247586191@qq.com>
2023-08-30 10:49:10 +0800

    :recycle: refactor(custom): uni.scss 修改为 main.js 中注册。

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import "tailwindcss/tailwind.css";
+import '@/uni.scss';
 import {
 	createSSRApp
 } from "vue";
 import App from "./App.vue";
-
-// import '@/mock/socket/server_ws'; // socket 服务器启动！
 
 // #ifdef VUE3
 export function createApp() {
